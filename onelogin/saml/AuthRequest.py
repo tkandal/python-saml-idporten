b459550a84c3026c97348a674d65f2cd92a08fe7
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2013-09-26 23:08:31 +0200

    Extract signing part of AuthRequest into a separate object.

--- onelogin/saml/AuthRequest.py
+++ onelogin/saml/AuthRequest.py
@@ -9,38 +9,13 @@ from datetime import datetime
 from lxml import etree
 from lxml.builder import ElementMaker
 
-
-def sign_request(urlencoded_request, private_key_file):
-    with tempfile.NamedTemporaryFile(delete=False) as output_file:
-        output_file.write(urlencoded_request.encode("utf-8"))
-        output_file.seek(0)
-
-        cmds = [
-            "openssl",
-            "sha1",
-            "-sign",
-            private_key_file,
-            output_file.name ]
-
-        proc = subp.Popen(
-            cmds,
-            stdout=subp.PIPE,
-            stderr=subp.PIPE)
-        out, err = proc.communicate()
-        print err
-
-    out = base64.b64encode(out)
-    return urllib.urlencode([("Signature", out),])
+from SignableRequest import SignableRequest
 
 
-
-class AuthRequest(object):
+class AuthRequest(SignableRequest):
     def __init__(self,
                  _clock=None,
                  _uuid=None,
-                 _zlib=None,
-                 _base64=None,
-                 _urllib=None,
                  **kwargs):
         """Create a URL string which can be used to redirect a samlp:AuthnRequest to the identity provider.
         Return a URL string containing the idp_sso_target_url and a deflated, base64-encoded, url-encoded (in that order) samlp:AuthnRequest XML element as the value of the SAMLRequest parameter.
@@ -114,34 +89,3 @@ class AuthRequest(object):
                                         )
         request_authn_context.append(authn_context_class_ref)
         self.document = authn_request
-
-
-
-
-    def get_signed_url(self, private_key_file, _zlib=None, _base64=None, _urllib=None):
-        if _zlib is None:
-            _zlib = zlib
-        if _base64 is None:
-            _base64 = base64
-        if _urllib is None:
-            _urllib = urllib
-
-        authn_request_string = etree.tostring(self.document, xml_declaration=True, encoding='UTF-8')
-
-
-        compressed_request = _zlib.compress(authn_request_string)
-        # Strip the first 2 bytes (header) and the last 4 bytes (checksum) to get the raw deflate
-        deflated_request = compressed_request[2:-4]
-        encoded_request = _base64.b64encode(deflated_request)
-        sig_alg = "http://www.w3.org/2000/09/xmldsig#rsa-sha1"
-        urlencoded_request = _urllib.urlencode(
-            [('SAMLRequest', encoded_request),
-             ('SigAlg', sig_alg)],)
-
-        signature = sign_request(urlencoded_request, private_key_file)
-
-        return '{url}?{query}&{Signature}'.format(
-            url=self.idp_sso_target_url,
-            query=urlencoded_request,
-            Signature=signature
-            )
---
+++ onelogin/saml/SignableRequest.py
@@ -0,0 +1,66 @@
+import zlib
+import base64
+import uuid
+import urllib
+import tempfile
+import subprocess as subp
+
+from datetime import datetime
+from lxml import etree
+from lxml.builder import ElementMaker
+
+
+def sign_request(urlencoded_request, private_key_file):
+    with tempfile.NamedTemporaryFile(delete=False) as output_file:
+        output_file.write(urlencoded_request.encode("utf-8"))
+        output_file.seek(0)
+
+        cmds = [
+            "openssl",
+            "sha1",
+            "-sign",
+            private_key_file,
+            output_file.name ]
+
+        proc = subp.Popen(
+            cmds,
+            stdout=subp.PIPE,
+            stderr=subp.PIPE)
+        out, err = proc.communicate()
+        print err
+
+    out = base64.b64encode(out)
+    return urllib.urlencode([("Signature", out),])
+
+
+class SignableRequest(object):
+    def __init__(self):
+        pass
+
+    def get_signed_url(self, private_key_file, _zlib=None, _base64=None, _urllib=None):
+        if _zlib is None:
+            _zlib = zlib
+        if _base64 is None:
+            _base64 = base64
+        if _urllib is None:
+            _urllib = urllib
+
+        authn_request_string = etree.tostring(self.document, xml_declaration=True, encoding='UTF-8')
+
+
+        compressed_request = _zlib.compress(authn_request_string)
+        # Strip the first 2 bytes (header) and the last 4 bytes (checksum) to get the raw deflate
+        deflated_request = compressed_request[2:-4]
+        encoded_request = _base64.b64encode(deflated_request)
+        sig_alg = "http://www.w3.org/2000/09/xmldsig#rsa-sha1"
+        urlencoded_request = _urllib.urlencode(
+            [('SAMLRequest', encoded_request),
+             ('SigAlg', sig_alg)],)
+
+        signature = sign_request(urlencoded_request, private_key_file)
+
+        return '{url}?{query}&{Signature}'.format(
+            url=self.idp_sso_target_url,
+            query=urlencoded_request,
+            Signature=signature
+            )
